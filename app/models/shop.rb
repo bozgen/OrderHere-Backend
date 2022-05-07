5ee60313ed9bc9dcc1ac0b853a880e78a1910eb1
bozgen <bilalozgen00@gmail.com>
2022-05-07 12:03:40 +0300

    minor fix

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,6 @@
 class Shop < ApplicationRecord
     validates :name, presence: true
     validates :shop_type_id, presence: true
-    before_save :set_url
 
     belongs_to :shop_type
     has_many :items
