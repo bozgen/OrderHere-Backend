a2202a6033f5459e772f2cc3be99b102c4ea0e18
bozgen <bilalozgen00@gmail.com>
2022-05-14 16:29:11 +0300

    debug 3

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -27,7 +27,7 @@ class OrderItemsController < ApplicationController
       create_order_item_params[:items].each do |item|
         @order_item = @table.order_items.new(item)
         if @order_item.save
-          itemsRow = Item.find(item[:id])
+          itemsRow = Item.find(create_order_item_params[:items][p][:id])
           newQuantity = itemsRow[:quantity] - item[:quantity]
           itemsRow.update_attribute(:quantity, newQuantity);
           render json: "item[:id] : #{item[:id]}"
