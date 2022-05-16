7667b59b49f1e4bbb9688d67aab6d3b9d7acf3a1
bozgen <bilalozgen00@gmail.com>
2022-05-16 17:44:15 +0300

    devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:create]
-  get   'login', to: 'sessions#new'
-  post  'login', to: 'sessions#create'
-
 
   get 'search', to: 'shops#search'
 
--- db/migrate/20220516143902_drop_tokens.rb
+++
@@ -1,5 +0,0 @@
-class DropTokens < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :tokens
-  end
-end
