5c3b278673e4d1914b128a1867e5b3474e9e8446
bozgen <bilalozgen00@gmail.com>
2022-05-16 15:30:19 +0300

    fix syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
     tokenizable
 
-    validates :username,            presence: true, unique: true
-    validates :email,               presence: true, unique: true
+    validates :username,            presence: true, :unique => true
+    validates :email,               presence: true, :unique => true
     validates :encrypted_password,  presence: true
-    validates :role,                presence: true, default: ROLES[:waiter]
+    validates :role,                presence: true, :default => ROLES[:waiter]
+    #Ex:- :default =>''
 
 
     private
