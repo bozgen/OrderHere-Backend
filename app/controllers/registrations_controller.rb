c6fd9cbdac36cd95c314ccb341e143ff4a5ee570
bozgen <bilalozgen00@gmail.com>
2022-05-16 21:48:18 +0300

    try fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,11 @@
 include ActionController::MimeResponds
 
 class RegistrationsController < Devise::RegistrationsController
-    
     respond_to :json
+    
     private
     def respond_with(resource, _opts = {})
-      resource.save ? register_success : register_failed
+      resource.persisted? ? register_success : register_failed
     end
     def register_success
       render json: { message: 'Signed up.' }
