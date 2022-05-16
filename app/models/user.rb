23d9945c8387c2468ae96d7d5bed858ca7fc5d4f
bozgen <bilalozgen00@gmail.com>
2022-05-16 15:55:59 +0300

    fix role default

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
     validates :username,            presence: true, uniqueness: true
     validates :email,               presence: true, uniqueness: true
     validates :encrypted_password,  presence: true
-    validates :role,                presence: true
-
 
     private
 
