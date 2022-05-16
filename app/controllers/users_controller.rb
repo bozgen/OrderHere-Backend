8cd01f07eb2e4a6793a7b71fc617d682607d0e6a
bozgen <bilalozgen00@gmail.com>
2022-05-16 15:59:34 +0300

    fix jbuilders users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render :show, status: :created, location: @user
+      render :create, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
