ea4f669accc44f53054abd46b9b3288b05171933
bozgen <bilalozgen00@gmail.com>
2022-05-16 21:30:06 +0300

    try fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
     respond_to :json
     private
     def respond_with(resource, _opts = {})
-      resource.persisted? ? register_success : register_failed
+      resource.save? ? register_success : register_failed
     end
     def register_success
       render json: { message: 'Signed up.' }
