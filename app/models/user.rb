facd87ec76e4f3efd10b5d0cfbb9c54b27601c5d
bozgen <bilalozgen00@gmail.com>
2022-05-16 17:12:44 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
     validates :username,            presence: true, uniqueness: true
     validates :email,               presence: true, uniqueness: true
-    validates :encrypted_password,  presence: true
+    validates :password_digest,     presence: true
 
     private
 
