16dfca101f84397e70d7bea328f8c521b3506d3b
bozgen <bilalozgen00@gmail.com>
2022-05-16 19:50:17 +0300

    try fix session issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, , :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :validatable
+         :recoverable, :validatable, :rememberable, :timeoutable
 
   validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
 
